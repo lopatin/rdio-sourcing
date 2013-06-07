dd2ce32373d438d0d673b9f7e6681b79685dd296
Alyssa Pohahau <alyssapohahau@gmail.com>
2013-06-07 14:47:57 -0700

    fixed indentations and stuff

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ def main():
 	authorization_url = rdio_api.authorization_url  # use this url for rdio button
 	# user will click rdio button, get pin from rdio, and give us pin
 	# pin = get pin from user
-	rdio_api.authorize_user_with_pin(pin)
-	rdio_api.exchange_request_token_for_access_token()
+	# rdio_api.authorize_user_with_pin(pin)
+	# rdio_api.exchange_request_token_for_access_token()
 	# ready to make authrorized calls, example call:
 	# rdio_api.add_to_collection(list_of_artists, type="Album")
 
 if __name__ == "__main__":
-    main()
+	main()
