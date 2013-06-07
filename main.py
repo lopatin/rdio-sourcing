a67eaa589d22a4276be80d833be243a9f478ae91
Jason Israel <jason.israel@rd.io>
2013-06-07 15:13:15 -0700

    Lastfm playlist scraper created

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
 	# user will click rdio button, get pin from rdio, and give us pin
 	# pin = get pin from user
 	rdio_api.authorize_user_with_pin(pin)
-    rdio_api.exchange_request_token_for_access_token()
+	rdio_api.exchange_request_token_for_access_token()
     # ready to make authrorized calls, example call:
     # rdio_api.add_to_collection(list_of_artists, type="Album")
 
