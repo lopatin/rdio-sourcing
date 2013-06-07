eadc5442a8b628b04d137f8dc09b43a7f4cc9dee
Alyssa Pohahau <alyssapohahau@gmail.com>
2013-06-07 14:40:51 -0700

    fixing tabs

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ def main():
 	# user will click rdio button, get pin from rdio, and give us pin
 	# pin = get pin from user
 	rdio_api.authorize_user_with_pin(pin)
-    rdio_api.exchange_request_token_for_access_token()
-    # ready to make authrorized calls, example call:
-    # rdio_api.add_to_collection(list_of_artists, type="Album")
+	rdio_api.exchange_request_token_for_access_token()
+	# ready to make authrorized calls, example call:
+	# rdio_api.add_to_collection(list_of_artists, type="Album")
 
 if __name__ == "__main__":
     main()
