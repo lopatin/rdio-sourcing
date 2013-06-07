e9a0c48f894dc7d84276354ba447bb747a5a2d18
Alex Lopatin <alex.lopatin@rd.io>
2013-06-07 13:07:58 -0700

    Added a privateconfig.json config file but you can't see it because it's in .gitignore

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express'),
 	app = express(),
-	pandora = require('./pandora');
+	pandora = require('./pandora'),
+	config = require('./privateconfig');
 
 app.use(express.static(__dirname+'/public'));
 
 app.listen(8080);
 
-pandora.fetchTracks('sitpomk@gmail.com', 'sasha16', function (result) {
+pandora.fetchTracks(config.pandora.username, config.pandora.password, function (result) {
 	console.log(result);
 });
