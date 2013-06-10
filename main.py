3f45aebf58bebcf8e801f749b6391864276952a1
Alyssa Pohahau <alyssapohahau@gmail.com>
2013-06-09 22:42:49 -0700

    removed debugging prints

--- main.py
+++ main.py
@@ -14,28 +14,20 @@ def main():
 	saved_token = rdio.token # a two element tuple
 	rdio.complete_authentication(verifier)
 
-	print "fetching last fm artists"
+	print "fetching last fm artists..."
 	lastfm = LastfmQuery.LastfmQuery()
 	lastfm_list_of_artists = lastfm.getAlbums("jaisrael")
 	
-	print "===================="
-	print "LIST OF ARTISTS"
-	print "===================="
-
+	print "adding artists to collection..." 
 	for artist in lastfm_list_of_artists:
-		print "===================="
-		print "ARTIST"
-		print artist
-		print "===================="
-		print "searching rdio for artist"
 		try:
 			response = rdio.call('search', {'query':artist, 'types':'Artist'})		
 		except UnicodeEncodeError:
 			print "unicode issue. skipping this artist"
 			continue
+	
 		artist_key = response['result']['results'][0]['key']
 		if artist_key is not None:
-			print "getting tracks for artist"
 			track_objs = rdio.call('getTracksForArtist', {'artist':artist_key})['result']
 			track_keys_unicode_list = [ track['key'] for track in track_objs ]
 			if track_keys_unicode_list is not None:
@@ -46,11 +38,7 @@ def main():
 					track_keys_str += u + ","
 				if track_keys_str.endswith(','):
 					track_keys_str = track_keys_str[:-1]
-				print "track keys adding to collection..."
-				print track_keys_str
-				print "adding artist's tracks to collection"
 				atc = rdio.call('addToCollection', {'keys':track_keys_str})
-				print atc
 
 def get_consumer_credentials():
 	f = open('config', 'r')
